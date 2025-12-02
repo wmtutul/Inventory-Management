65246038b25bc3e33d4862ca6d09e4b0681e1cf6
wmtutul <wmtutul@gmail.com>
2025-12-02 20:29:14 +0600

    frontend sidebar ---

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
 import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Root from './utils/Root';
 import Login from './pages/Login';
 import ProtectedRoutes from './utils/ProtectedRoutes';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Root/>}/>
-        <Route path='/admin/dashboard' element = {<ProtectedRoutes requireRole={["admin"]}>
-          <h1>admin dashboard</h1>
-        </ProtectedRoutes>} />
-        <Route path='/customer/dashboard' element={<h1>customer dashboard</h1>}/>
-        <Route path='/login' element={<Login/>} />
+        <Route path='/' element={<Root />} />
+
+        <Route 
+          path='/admin-dashboard' 
+          element={
+            <ProtectedRoutes requireRole={["admin"]}>
+              <Dashboard/>
+            </ProtectedRoutes>
+            }
+        >
+          <Route index element={<h1>Summary of dashboard</h1>} />   
+
+        </Route>
+
+        <Route path='/customer/dashboard' element={<h1>customer dashboard</h1>} />
+        <Route path='/login' element={<Login />} />
         <Route path='/unauthorized' element={<p className='font-bold text-3xl mt-20 ml-20'>Unauthorized</p>} />
       </Routes>
     </Router>
