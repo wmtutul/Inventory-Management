86f17655d4b91ff548d5fcda1544eead8006fe26
wmtutul <wmtutul@gmail.com>
2025-12-03 17:56:14 +0600

    Frontend Sidebar design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,13 @@ const App = () => {
             }
         >
           <Route index element={<h1>Summary of dashboard</h1>} />   
-
+          <Route path='categories' element={<h1>Categories</h1>} />   
+          <Route path='products' element={<h1>Products</h1>} />   
+          <Route path='suppliers' element={<h1>Suppliers</h1>} />   
+          <Route path='orders' element={<h1>Orders</h1>} />   
+          <Route path='users' element={<h1>Users</h1>} />   
+          <Route path='profile' element={<h1>Profile</h1>} />   
+          <Route path='logout' element={<h1>Logout</h1>} />   
         </Route>
 
         <Route path='/customer/dashboard' element={<h1>customer dashboard</h1>} />
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import {FaTable, FaHome, FaBox, FaTruck, FaShoppingCart, FaUsers, FaCog, FaSignOutAlt} from "react-icons/fa";
+import {NavLink} from 'react-router';
 
 const Sidebar = () => {
+
+  const menuItems = [
+    {name: "Dashboard", path: "/admin-dashboard", icon: <FaHome/>, isParent: true},
+    {name: "Catagories", path: "/admin-dashboard/categories", icon: <FaTable/>, isParent: false},
+    {name: "Products", path: "/admin-dashboard/products", icon: <FaBox/>, isParent: false},
+    {name: "Suppliers", path: "/admin-dashboard/suppliers", icon: <FaTruck/>, isParent: false},
+    {name: "Orders", path: "/admin-dashboard/orders", icon: <FaShoppingCart/>, isParent: false},
+    {name: "Users", path: "/admin-dashboard/users", icon: <FaUsers/>, isParent: false},
+    {name: "Profile", path: "/admin-dashboard/profile", icon: <FaCog/>, isParent: false},
+    {name: "Logout", path: "/admin-dashboard/logout", icon: <FaSignOutAlt/>, isParent: false},
+  ] 
+
+
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col h-screen  bg-black text-white w-16 md:w-64 fixed'>
+      <div className='h-16 mt-4 flex flex-items justify-center'>
+        <span className='hidden md:block text-xl font-bold'>Inventory MS</span>
+        <span className='md:hidden text-xl font-bold '>IMS</span>
+      </div>
+
+      <div>
+        <ul className='space-y-2 p-2'>
+          {
+            menuItems.map((item) => (
+              <li key={item.name}>
+                <NavLink
+                  end={item.isParent}
+                  className={({isActive}) => (isActive ? "bg-gray-700" : " ") + " flex items-center p-2 rounded-md  hover:bg-gray-700 transition duration-200"} 
+                  to={item.path}
+                >
+                  <span className='text-xl'>{item.icon}</span>
+                  <span className='ml-2 hidden md:block'>{item.name}</span>
+                </NavLink>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
   )
+
 }
 
 export default Sidebar
+ 
+
